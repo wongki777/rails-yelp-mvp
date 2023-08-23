c5ecf8f0380e7a4856760d245b02a0b64df917dd
Olivia Wong <oliviawong@outlook.com>
2023-08-23 23:00:10 +0200

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:index, :create]
+  end
 end
+
+# A visitor can see the list of all restaurants.
+# GET "restaurants"
+# A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
+# GET "restaurants/new"
+# POST "restaurants"
+# A visitor can see the details of a restaurant, with all the reviews related to the restaurant.
+# GET "restaurants/38"
+# A visitor can add a new review to a restaurant
+# GET "restaurants/38/reviews/new"
+# POST "restaurants/38/reviews"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+# dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", rating: 5}
+# pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", rating: 4}
+
+bristol = {name: "Epicure", category: "french", address: "75008 Paris", phone_number: "01234 578990"}
+milano = {name: "Rome", category: "italian", address: "75007 Italy", phone_number: "01234 578991"}
+nagano = {name: "Tokyo", category: "japanese", address: "75006 Japan", phone_number: "01234 578992"}
+xian = {name: "Beijing", category: "chinese", address: "75005 Chinese", phone_number: "01234 578993"}
+bruges = {name: "Brussels", category: "belgian", address: "75004 Belgium", phone_number: "01234 578994"}
+
+
+[bristol, milano, nagano, xian, bruges].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
